8d7020ed98322311d45a5c8fd9b0cf48256ec584
Zoey <z@zoey.ninja>
2025-11-14 16:00:52 -0500

    added link to story page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,20 @@
 	import { _ } from 'svelte-i18n';
 	import SelectTargets from '$lib/components/SelectTargets.svelte';
 	import { authStore } from '../stores/authStore';
+	import { goto } from '$app/navigation';
+	import { resolve } from '$app/paths';
 </script>
 
 <div class="mx-auto max-w-md p-6">
 	{#if $authStore.isLoggedIn}
-		<h1 class="mb-6 text-center text-2xl font-bold">Logged in</h1>
+		<div class="mb-6 flex items-center justify-between">
+			<button
+				on:click={() => goto(resolve('/story'))}
+				type="button"
+				class="m-auto mx-auto btn preset-outlined-primary-500"
+				>{$_('navigation.generateStory')}</button
+			>
+		</div>
 	{:else}
 		<SelectTargets />
 	{/if}
