d26b616f8faa7259d1bca3accecaf577c6b4bc13
zoey <z@zoey.ninja>
2025-12-02 17:50:57 +0000

    feat: add side drawer for navigation

---
+++ src/lib/components/Drawer.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { XIcon } from '@lucide/svelte'
+  import { Dialog, Portal } from '@skeletonlabs/skeleton-svelte'
+  import { MenuIcon } from '@lucide/svelte'
+
+  // Animations:
+  const animBackdrop =
+    'transition transition-discrete opacity-0 starting:data-[state=open]:opacity-0 data-[state=open]:opacity-100'
+  const animModal =
+    'transition transition-discrete opacity-0 -translate-x-full starting:data-[state=open]:opacity-0 starting:data-[state=open]:-translate-x-full data-[state=open]:opacity-100 data-[state=open]:translate-x-0'
+</script>
+
+<Dialog>
+  <Dialog.Trigger class="btn-icon btn-icon-lg hover:preset-tonal"><MenuIcon /></Dialog.Trigger>
+  <Portal>
+    <Dialog.Backdrop
+      class="bg-surface-50-950/50 fixed inset-0 z-50 transition transition-discrete {animBackdrop}"
+    />
+    <Dialog.Positioner class="fixed inset-0 z-50 flex justify-start">
+      <Dialog.Content
+        class="card bg-surface-100-900 h-screen w-sm space-y-4 p-4 shadow-xl {animModal}"
+      >
+        <header class="flex items-center justify-between">
+          <Dialog.Title class="text-2xl font-bold">StoryLingo</Dialog.Title>
+          <Dialog.CloseTrigger class="btn-icon preset-tonal">
+            <XIcon />
+          </Dialog.CloseTrigger>
+        </header>
+        <p>A slide out drawer panel.</p>
+      </Dialog.Content>
+    </Dialog.Positioner>
+  </Portal>
+</Dialog>
--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   import { resolve } from '$app/paths'
-  import { MenuIcon } from '@lucide/svelte'
   import { AppBar } from '@skeletonlabs/skeleton-svelte'
   import LanguageSelector from './LanguageSelector.svelte'
+  import Drawer from './Drawer.svelte'
 </script>
 
 <AppBar>
   <AppBar.Toolbar class="grid-cols-[auto_1fr_auto]">
     <AppBar.Lead>
-      <button type="button" class="btn-icon btn-icon-lg hover:preset-tonal"><MenuIcon /></button>
+      <Drawer />
     </AppBar.Lead>
     <AppBar.Headline>
       <a href={resolve('/')} class="hover:text-primary-500 text-2xl">StoryLingo</a>
