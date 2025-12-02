9449770143f9822c0269537cffa80089b9532383
zoey <z@zoey.ninja>
2025-12-02 21:01:36 +0000

    feat: move authentication and target select logic to story page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,17 @@
 <script lang="ts">
   import { _ } from 'svelte-i18n'
-  import SelectTargets from '$lib/components/SelectTargets.svelte'
-  import { authStore } from '$lib/stores/authStore'
   import { goto } from '$app/navigation'
   import { resolve } from '$app/paths'
 </script>
 
+<!-- TODO: add descriptive text -->
 <div class="mx-auto max-w-md p-6">
-  {#if $authStore.isLoggedIn}
-    <div class="mb-6 flex items-center justify-between">
-      <button
-        on:click={() => goto(resolve('/story'))}
-        type="button"
-        class="btn preset-outlined-primary-500 m-auto mx-auto"
-        >{$_('navigation.generateStory')}</button
-      >
-    </div>
-  {:else}
-    <SelectTargets />
-  {/if}
+  <div class="mb-6 flex items-center justify-between">
+    <button
+      on:click={() => goto(resolve('/story'))}
+      type="button"
+      class="btn preset-outlined-primary-500 m-auto mx-auto"
+      >{$_('navigation.generateStory')}</button
+    >
+  </div>
 </div>
